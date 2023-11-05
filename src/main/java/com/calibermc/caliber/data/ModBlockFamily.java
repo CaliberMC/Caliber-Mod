7a0c32e45762f1a36f8ee768d83f04bff432c750
Charm8er <josh@luttmer.us>
2023-11-04 20:54:09 -0700

    Added Copy BlockState Key. Added ReachDistance Key.  Added New Creative Inventory Layout.

---
+++ src/main/java/com/calibermc/caliber/data/ModBlockFamily.java
@@ -0,0 +1,297 @@
+package com.calibermc.caliber.data;
+
+import com.google.common.collect.Maps;
+import net.minecraft.world.level.block.Block;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+import java.util.Optional;
+
+public class ModBlockFamily {
+    private final Block baseBlock;
+    final Map<ModBlockFamily.Variant, Block> variants = Maps.newHashMap();
+    boolean generateModel = true; //default to true
+    boolean generateRecipe = true; //default to true
+    @Nullable
+    String recipeGroupPrefix;
+    @Nullable
+    String recipeUnlockedBy;
+
+    ModBlockFamily(Block pBaseBlock) {
+        this.baseBlock = pBaseBlock;
+    }
+
+    public Block getBaseBlock() {
+        return this.baseBlock;
+    }
+
+    public Map<ModBlockFamily.Variant, Block> getVariants() {
+        return this.variants;
+    }
+
+    public Block get(ModBlockFamily.Variant pVariant) {
+        return this.variants.get(pVariant);
+    }
+
+    public boolean shouldGenerateModel() {
+        return this.generateModel;
+    }
+
+    public boolean shouldGenerateRecipe() {
+        return this.generateRecipe;
+    }
+
+    public Optional<String> getRecipeGroupPrefix() {
+        return StringUtils.isBlank(this.recipeGroupPrefix) ? Optional.empty() : Optional.of(this.recipeGroupPrefix);
+    }
+
+    public Optional<String> getRecipeUnlockedBy() {
+        return StringUtils.isBlank(this.recipeUnlockedBy) ? Optional.empty() : Optional.of(this.recipeUnlockedBy);
+    }
+
+    public static class Builder {
+        private final ModBlockFamily family;
+
+        public Builder(Block pBaseBlock) {
+            this.family = new ModBlockFamily(pBaseBlock);
+        }
+
+        public ModBlockFamily getFamily() {
+            return this.family;
+        }
+
+        public ModBlockFamily.Builder arch(Block pArchBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.ARCH, pArchBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder arch_half(Block pArch_HalfBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.ARCH_HALF, pArch_HalfBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder arch_large(Block pArch_LargeBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.ARCH_LARGE, pArch_LargeBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder arch_large_half(Block pArch_Large_HalfBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.ARCH_LARGE_HALF, pArch_Large_HalfBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder arch_small(Block pArch_SmallBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.ARCH_SMALL, pArch_SmallBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder arch_small_half(Block pArch_Small_HalfBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.ARCH_SMALL_HALF, pArch_Small_HalfBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder arrowslit(Block pArrowslitBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.ARROWSLIT, pArrowslitBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder balustrade(Block pBalustradeBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.BALUSTRADE, pBalustradeBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder button(Block pButtonBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.BUTTON, pButtonBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder capital(Block pCapitalBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.CAPITAL, pCapitalBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder chiseled(Block pChiseledBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.CHISELED, pChiseledBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder cracked(Block pCrackedBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.CRACKED, pCrackedBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder column(Block pColumnBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.COLUMN, pColumnBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder corner(Block pCornerBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.CORNER, pCornerBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder corner_slab(Block pCorner_SlabBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.CORNER_SLAB, pCorner_SlabBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder corner_slab_vertical(Block pCorner_Slab_VerticalBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.CORNER_SLAB_VERTICAL, pCorner_Slab_VerticalBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder cut(Block pCutBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.CUT, pCutBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder door(Block pDoorBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.DOOR, pDoorBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder eighth(Block pEighthBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.EIGHTH, pEighthBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder fence(Block pFenceBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.FENCE, pFenceBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder fenceGate(Block pFenceGateBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.FENCE_GATE, pFenceGateBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder quarter(Block pQuarterBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.QUARTER, pQuarterBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder quarter_vertical(Block pQuarter_VerticalBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.QUARTER_VERTICAL, pQuarter_VerticalBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder sign(Block pSignBlock, Block pWallSignBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.SIGN, pSignBlock);
+            this.family.variants.put(ModBlockFamily.Variant.WALL_SIGN, pWallSignBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder slab(Block pSlabBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.SLAB, pSlabBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder slab_vertical(Block pSlab_VerticalBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.SLAB_VERTICAL, pSlab_VerticalBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder stairs(Block pStairsBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.STAIRS, pStairsBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder pressurePlate(Block pPressurePlateBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.PRESSURE_PLATE, pPressurePlateBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder polished(Block pPolishedBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.POLISHED, pPolishedBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder trapdoor(Block pTrapdoorBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.TRAPDOOR, pTrapdoorBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder wall(Block pWallBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.WALL, pWallBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder window(Block pWindowBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.WINDOW, pWindowBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder window_half(Block pWindow_HalfBlock) {
+            this.family.variants.put(ModBlockFamily.Variant.WINDOW_HALF, pWindow_HalfBlock);
+            return this;
+        }
+
+        public ModBlockFamily.Builder dontGenerateModel() {
+            this.family.generateModel = false;
+            return this;
+        }
+
+        public ModBlockFamily.Builder dontGenerateRecipe() {
+            this.family.generateRecipe = false;
+            return this;
+        }
+
+        public ModBlockFamily.Builder recipeGroupPrefix(String pRecipeGroupPrefix) {
+            this.family.recipeGroupPrefix = pRecipeGroupPrefix;
+            return this;
+        }
+
+        public ModBlockFamily.Builder recipeUnlockedBy(String pRecipeUnlockedBy) {
+            this.family.recipeUnlockedBy = pRecipeUnlockedBy;
+            return this;
+        }
+    }
+
+    public static enum Variant {
+        ARCH("arch"),
+        ARCH_HALF("arch_half"),
+        ARCH_LARGE("arch_large"),
+        ARCH_LARGE_HALF("arch_large_half"),
+        ARCH_SMALL("arch_small"),
+        ARCH_SMALL_HALF("arch_small_half"),
+        ARROWSLIT("arrowslit"),
+        BALUSTRADE("balustrade"),
+        BUTTON("button"),
+        CAPITAL("capital"),
+        CHISELED("chiseled"),
+        CRACKED("cracked"),
+        COLUMN("column"),
+        CORNER("corner"),
+        CORNER_SLAB("corner_slab"),
+        CORNER_SLAB_VERTICAL("corner_slab_vertical"),
+        CUT("cut"),
+        DOOR("door"),
+        EIGHTH("eighth"),
+        FENCE("fence"),
+        FENCE_GATE("fence_gate"),
+        QUARTER("quarter"),
+        QUARTER_VERTICAL("quarter_vertical"),
+        SIGN("sign"),
+        SLAB("slab"),
+        SLAB_VERTICAL("slab_vertical"),
+        STAIRS("stairs"),
+        PRESSURE_PLATE("pressure_plate"),
+        POLISHED("polished"),
+        TRAPDOOR("trapdoor"),
+        WALL("wall"),
+        WALL_SIGN("wall_sign"),
+        WINDOW("window"),
+        WINDOW_HALF("window_half");
+
+        private final String name;
+
+        private Variant(String pVariantName) {
+            this.name = pVariantName;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+}
