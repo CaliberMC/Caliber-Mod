a131279d9b22c72ac7df8a9f51a572dda7140013
Charm8er <josh@luttmer.us>
2023-11-18 21:00:15 -0800

    Updated Grass Layer texture coloring

--- src/main/java/com/calibermc/caliber/client/ColorHandler.java
+++ src/main/java/com/calibermc/caliber/client/ColorHandler.java
@@ -1,13 +1,13 @@
 package com.calibermc.caliber.client;
 
 import com.calibermc.caliber.block.ModBlocks;
+import net.minecraft.world.level.GrassColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.renderer.BiomeColors;
-
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ColorHandler {
@@ -18,10 +18,11 @@ public class ColorHandler {
 
         blockColors.register((state, reader, pos, tintIndex) -> {
             if (tintIndex == 0 && reader != null && pos != null) {
-                // Apply biome color only to the top of the block
+                // Apply biome color to the block
                 return BiomeColors.getAverageGrassColor(reader, pos);
+            } else {
+                return GrassColor.get(0.5D, 1.0D);
             }
-            return -1; // Do not apply biome color to other parts of the block
         }, ModBlocks.GRASS_LAYER.get());
     }
 }
