6279247124122720fcf48a21e22c9f8f8cd8e448
Charm8er <josh@luttmer.us>
2023-11-21 17:55:31 -0800

    Added all remaining variants of all existing Tudor Blocks

--- src/main/java/com/calibermc/caliber/item/ModItems.java
+++ src/main/java/com/calibermc/caliber/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.calibermc.caliber.item;
 
 import com.calibermc.caliber.Caliber;
+import com.calibermc.caliber.item.custom.CycleItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -10,6 +11,9 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Caliber.MOD_ID);
+
+    public static final RegistryObject<CycleItem> HAMMER = ITEMS.register("hammer",
+            () -> new CycleItem(new Item.Properties().tab(CreativeTabs.CALIBER_TAB)));
 
     public static final RegistryObject<Item> TIN_INGOT = ITEMS.register("tin_ingot",
             () -> new Item(new Item.Properties().tab(CreativeTabs.CALIBER_TAB)));
