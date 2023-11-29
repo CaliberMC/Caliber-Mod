2c08e0963fcd40bca2097700127f6c4496188d22
Charm8er <josh@luttmer.us>
2023-11-28 19:32:35 -0800

    Added Null Checks on BlockItemMixin.

--- src/main/java/com/calibermc/caliber/mixin/BlockItemMixin.java
+++ src/main/java/com/calibermc/caliber/mixin/BlockItemMixin.java
@@ -6,10 +6,13 @@ import com.calibermc.caliber.config.CaliberCommonConfigs;
 import com.calibermc.caliber.util.ModBlockStateProperties;
 import com.calibermc.caliber.util.player.IPlayerExtended;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.Property;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,25 +21,52 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(BlockItem.class)
 public class BlockItemMixin {
 
+//    @Inject(method = "getPlacementState(Lnet/minecraft/world/item/context/BlockPlaceContext;)Lnet/minecraft/world/level/block/state/BlockState;", at = @At("RETURN"), cancellable = true)
+//    public void mixin$getPlacementState(BlockPlaceContext pContext, CallbackInfoReturnable<BlockState> cir) {
+//        if (pContext.getPlayer() instanceof IPlayerExtended ex) {
+//            BlockState state = cir.getReturnValue();
+//            if ((state.hasProperty(BlockStateProperties.LAYERS) || state.hasProperty(ModBlockStateProperties.FIVE_LAYERS)) && pContext.getPlayer() instanceof ServerPlayer) {
+//                boolean slab = CaliberCommonConfigs.MODE_BLOCKSTATE.get() == 0;
+//                if (ex.caliber$additionalPressed()) {
+//                    slab = !slab;
+//                }
+//                if (slab) {
+//                    int oldSlabVal = state.getValue(state.hasProperty(ModBlockStateProperties.FIVE_LAYERS) ?
+//                            ModBlockStateProperties.FIVE_LAYERS : BlockStateProperties.LAYERS);
+//                    int slabVal = 3;
+//                    if (state.getBlock() instanceof SlabLayerBlock || state.getBlock() instanceof VerticalSlabLayerBlock) {
+//                        slabVal = 4;
+//                    }
+//                    if (oldSlabVal != slabVal) {
+//                        cir.setReturnValue(state.setValue(state.hasProperty(ModBlockStateProperties.FIVE_LAYERS) ?
+//                                ModBlockStateProperties.FIVE_LAYERS : BlockStateProperties.LAYERS, slabVal));
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
     @Inject(method = "getPlacementState(Lnet/minecraft/world/item/context/BlockPlaceContext;)Lnet/minecraft/world/level/block/state/BlockState;", at = @At("RETURN"), cancellable = true)
     public void mixin$getPlacementState(BlockPlaceContext pContext, CallbackInfoReturnable<BlockState> cir) {
-        if (pContext.getPlayer() instanceof IPlayerExtended ex) {
+        Player player = pContext.getPlayer();
+        if (player instanceof IPlayerExtended ex) {
             BlockState state = cir.getReturnValue();
-            if ((state.hasProperty(BlockStateProperties.LAYERS) || state.hasProperty(ModBlockStateProperties.FIVE_LAYERS)) && pContext.getPlayer() instanceof ServerPlayer) {
+            if (state != null && ((state.hasProperty(BlockStateProperties.LAYERS) || state.hasProperty(ModBlockStateProperties.FIVE_LAYERS)) && player instanceof ServerPlayer)) {
                 boolean slab = CaliberCommonConfigs.MODE_BLOCKSTATE.get() == 0;
                 if (ex.caliber$additionalPressed()) {
                     slab = !slab;
                 }
                 if (slab) {
-                    int oldSlabVal = state.getValue(state.hasProperty(ModBlockStateProperties.FIVE_LAYERS) ?
-                            ModBlockStateProperties.FIVE_LAYERS : BlockStateProperties.LAYERS);
+                    Property<Integer> property = state.hasProperty(ModBlockStateProperties.FIVE_LAYERS) ? ModBlockStateProperties.FIVE_LAYERS : BlockStateProperties.LAYERS;
+                    int oldSlabVal = state.getValue(property);
                     int slabVal = 3;
-                    if (state.getBlock() instanceof SlabLayerBlock || state.getBlock() instanceof VerticalSlabLayerBlock) {
+                    Block block = state.getBlock();
+                    if (block instanceof SlabLayerBlock || block instanceof VerticalSlabLayerBlock) {
                         slabVal = 4;
                     }
                     if (oldSlabVal != slabVal) {
-                        cir.setReturnValue(state.setValue(state.hasProperty(ModBlockStateProperties.FIVE_LAYERS) ?
-                                ModBlockStateProperties.FIVE_LAYERS : BlockStateProperties.LAYERS, slabVal));
+                        cir.setReturnValue(state.setValue(property, slabVal));
                     }
                 }
             }
