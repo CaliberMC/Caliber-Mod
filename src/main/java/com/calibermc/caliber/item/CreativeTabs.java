a4d4c4a5aa863d441767307cdcdbf4bab6628a78
Charm8er <josh@luttmer.us>
2024-02-08 20:31:49 -0800

    Removed unused translations from en_us.json.  Updated version number in build.gradle to use mod_version-minecraft_version from gradle.properties.  Removed unused code in CreativeTabs.

--- src/main/java/com/calibermc/caliber/item/CreativeTabs.java
+++ src/main/java/com/calibermc/caliber/item/CreativeTabs.java
@@ -2,6 +2,7 @@ package com.calibermc.caliber.item;
 
 import com.calibermc.caliber.Caliber;
 import com.calibermc.caliber.block.CaliberBlocks;
+import net.minecraft.core.NonNullList;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -32,60 +33,4 @@ public class CreativeTabs {
     public static void register(IEventBus eventBus) {
         CREATIVE_TABS.register(eventBus);
     }
-
-//    private static final String[] GRANITE = new String[]{"black_granite", "brown_granite", "gray_granite", "pink_granite",
-//            "granite", "red_granite", "white_granite"};
-//    private static final String[] LIMESTONE = new String[]{"dark_limestone", "light_limestone", "pink_limestone",
-//            "tan_limestone", "stone", "limestone"};
-//    private static final String[] MARBLE = new String[]{"black_marble", "gray_marble", "pink_marble",
-//            "red_marble", "white_marble"};
-//    private static final String[] SANDSTONE = new String[]{"brown_sandstone", "orange_sandstone", "red_sandstone", "sandstone"};
-
-    // Sorted
-    private static List<Item> getItems() {
-        return ForgeRegistries.ITEMS.getEntries().stream().map(Map.Entry::getValue).sorted((o, o1) -> {
-            ResourceLocation str1 = ForgeRegistries.ITEMS.getKey(o);
-            ResourceLocation str2 = ForgeRegistries.ITEMS.getKey(o1);
-            assert str1 != null && str2 != null;
-            return str1.compareTo(str2);
-        }).toList();
-    }
-
-    // Sorted or Not Sorted via Config
-//        private static List<Item> getItems() {
-//            boolean shouldSort = CaliberClientConfigs.SORT_CREATIVE_INVENTORY.get();
-////            boolean shouldSort = false;
-//
-//            // Retrieve all registered items as a stream of Map.Entry objects.
-//            List<Item> items = ForgeRegistries.ITEMS.getEntries().stream()
-//                    // Map each Map.Entry to its associated item.
-//                    .map(Map.Entry::getValue)
-//                    // Collect the items into a list.
-//                    .toList();
-//
-//            // Conditionally sort the list if shouldSort is true.
-//            if (shouldSort) {
-//                items.sort((item1, item2) -> {
-//                    ResourceLocation loc1 = ForgeRegistries.ITEMS.getKey(item1);
-//                    ResourceLocation loc2 = ForgeRegistries.ITEMS.getKey(item2);
-//
-//                    // Use compareTo to compare the ResourceLocation strings.
-//                    assert loc1 != null && loc2 != null;
-//                    return loc1.compareTo(loc2);
-//                });
-//            }
-//
-//            return items;
-//        }
-
-
-    // Unsorted
-//        private static List<Item> getItems() {
-//            // Retrieve all registered items as a stream of Map.Entry objects.
-//            return ForgeRegistries.ITEMS.getEntries().stream()
-//                    // Map each Map.Entry to its associated item.
-//                    .map(Map.Entry::getValue)
-//                    // Collect the items into a list without sorting.
-//                    .toList();
-//        }
 }
